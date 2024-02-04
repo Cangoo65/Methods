86e7d6372c632b87e6950e84c1d142758e1e15d4
Cangoo65 <caneraytekin06@hotmail.com>
2024-02-04 21:23:40 +0300

    Proje dosyası ekle.

---
+++ Methodlar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class Methodlar
+    {
+        public int ArtirveTopla(int x, int y)
+        {
+            x += 1;
+            y += 1;
+            return x + y;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Methods;
+
+int a = 4;
+int b = 5;
+
+Console.WriteLine(a+b);
+
+int sonuc= Topla(a,b);
+
+Console.WriteLine(sonuc);
+
+Methodlar Ornek=new Methodlar();
+int sonucArt=Ornek.ArtirveTopla(a,b);
+
+Console.WriteLine(sonucArt);
+static int Topla(int deger1,int deger2)
+{
+    return deger1 + deger2;
+}
+Console.Read();
